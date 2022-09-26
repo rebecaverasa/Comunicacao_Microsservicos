47070da3f25b4ecc266f0f1ef1eed93f2a32d0de
Rebeca Veras <rebecaverasa@gmail.com>
2022-09-26 15:24:29 -0300

    Sequelize set up Initial data created for the service

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,17 +1,21 @@
 import express from "express";
 
+import * as db from "./scr/config/db/initialData.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
+db.createInitialData();
+
 app.get("/api/status", (req, res) => {
-    return res.status(200).json({
-        service: "Auth-API",
-        status: "up",
-        httpStatus: 200,
-    });
+  return res.status(200).json({
+    service: "Auth-API",
+    status: "up",
+    httpStatus: 200,
+  });
 });
 
 app.listen(PORT, () => {
-    console.info(`Server started successfully at port ${PORT}`);
+  console.info(`Server started successfully at port ${PORT}`);
 });
--- auth-api/scr/config/db/dbConfig.js
+++ auth-api/scr/config/db/dbConfig.js
@@ -1,26 +1,26 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize ("auth-db","postgres", "cvtpc", {
-    host: "localhost",
-    dialect: "postgres",
-    quoteIdentifiers: false,
-    define: {
-        syncOnAssociation: true,
-        timestamps: false,
-        underscored: true,
-        underscoredAll: true,
-        freezeTableName: true
-    },
+const sequelize = new Sequelize("auth-db", "postgres", "password", {
+  host: "localhost",
+  dialect: "postgres",
+  quoteIdentifiers: false,
+  define: {
+    syncOnAssociation: true,
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
+    freezeTableName: true,
+  },
 });
 
 sequelize
-.authenticate().then(() => {
-    console.info("Connection has been stablished!");
-})
-.catch((err) => {
+  .authenticate()
+  .then(() => {
+    console.info("Connection has been stablished");
+  })
+  .catch((err) => {
     console.error("Unable to connect to the database.");
     console.error(err.message);
-});
+  });
 
 export default sequelize;
-
