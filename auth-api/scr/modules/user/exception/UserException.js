0c663053170934ba582e5dac69525d978d3cc717
Rebeca Veras <rebecaverasa@gmail.com>
2022-09-27 12:02:32 -0300

    Exception layer created

---
+++ auth-api/scr/modules/user/exception/UserException.js
@@ -0,0 +1,12 @@
+class UserException extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+        this.message = message;
+        this.name = this.constructor.name;
+        Error.name = this.constructor.name;
+        Error.captureStackTrace(this.this.constructor);
+    }
+}
+
+module.exports = UserException;
--- auth-api/scr/modules/user/service/UserService.js
+++ auth-api/scr/modules/user/service/UserService.js
@@ -1,35 +1,43 @@
 import userRepository from "../repository/userRepository";
 import * as httpStatus from "../../../config/constants/httpStatus";
+import UserException from "../exception/UserException";
 
 class UserService {
   async findByEmail(req) {
     try {
       const { email } = req.params;
       this.validateRequestData(email);
-      let user = userRepository.findByEmail(email);
-      if (!user) {
-
-      }
+      let user = await userRepository.findByEmail(email);
+      this.validateUserNotFound(user);
       return {
         status: httpStatus.SUCESS,
         user: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
+          id: user.id,
+          name: user.name,
+          email: user.email,
         },
       };
     } catch (err) {
-        return {
-            status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
-            message: err.status
-        }
+      return {
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message,
+      };
     }
   }
 
   validateRequestData(email) {
     if (!email) {
-      throw new Error("User email was not informed.");
+      throw new UserException(
+        httpStatus.BAD_REQUEST,
+        "User email was not informed."
+      );
     }
+  }
+
+  validateUserNotFound(user) {
+   if (!user) {
+    throw new Error(httpStatus.BAD_REQUEST, "User was not found.");
+   } 
   }
 }
 
