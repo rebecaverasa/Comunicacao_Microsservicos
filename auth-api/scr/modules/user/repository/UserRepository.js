ed5ecc71c9495ab0a387e03c3094aa64b858661e
Rebeca Veras <rebecaverasa@gmail.com>
2022-09-27 11:46:02 -0300

    UserRepository and UserService created

---
+++ auth-api/scr/modules/user/repository/UserRepository.js
@@ -0,0 +1,23 @@
+import User from "../model/User";
+
+class UserRepository {
+  async findById(id) {
+    try {
+      return await User.findOne({ where: { id } });
+    } catch (err) {
+      console.error(err.message);
+      return null;
+    }
+  }
+
+  async findByEmail(email) {
+    try {
+      return await User.findOne({ where: { email } });
+    } catch (err) {
+      console.error(err.message);
+      return null;
+    }
+  }
+}
+
+export default new UserRepository();
---
+++ auth-api/scr/modules/user/service/UserService.js
@@ -0,0 +1,36 @@
+import userRepository from "../repository/userRepository";
+import * as httpStatus from "../../../config/constants/httpStatus";
+
+class UserService {
+  async findByEmail(req) {
+    try {
+      const { email } = req.params;
+      this.validateRequestData(email);
+      let user = userRepository.findByEmail(email);
+      if (!user) {
+
+      }
+      return {
+        status: httpStatus.SUCESS,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+      };
+    } catch (err) {
+        return {
+            status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+            message: err.status
+        }
+    }
+  }
+
+  validateRequestData(email) {
+    if (!email) {
+      throw new Error("User email was not informed.");
+    }
+  }
+}
+
+export default new UserService();
