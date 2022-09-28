b91632e3f78ae3fd8d0f332db321695f150951a1
Rebeca Veras <rebecaverasa@gmail.com>
2022-09-28 14:27:30 -0300

    Authentication with JWT setted up

--- auth-api/scr/modules/user/routes/UserRoutes.js
+++ auth-api/scr/modules/user/routes/UserRoutes.js
@@ -5,6 +5,8 @@ import UserController from "../controller/UserController.js";
 const router = new Router();
 
 router.get("/api/user/email/:email", UserController.findByEmail);
+router.post("/api/user/auth/", UserController.getAcessToken);
+
 // /api/user/email/nomedapessoa@gmail/com
 
 export default router;
--- auth-api/scr/modules/user/service/UserService.js
+++ auth-api/scr/modules/user/service/UserService.js
@@ -1,6 +1,10 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import UserRepository from "../repository/UserRepository.js";
 import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
+import * as secrets from "../../../config/constants/secrets.js";
 
 class UserService {
   async findByEmail(req) {
@@ -35,9 +39,50 @@ class UserService {
   }
 
   validateUserNotFound(user) {
-   if (!user) {
-    throw new Error(httpStatus.BAD_REQUEST, "User was not found.");
-   } 
+    if (!user) {
+      throw new Error(httpStatus.BAD_REQUEST, "User was not found.");
+    }
+  }
+
+  async getAcessToken(req) {
+    try {
+      const { email, password } = req.body;
+      this.validateAcessTokenData(email, password);
+      let user = await UserRepository.findByEmail(email);
+      this.validateUserNotFound(user);
+      await this.validatePassword(password, user.password);
+      const authUser = { id: user.id, name: user.name, email: user.email };
+      const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, {
+        expiresIn: "1d",
+      });
+      return {
+        status: httpStatus.SUCESS,
+        accessToken,
+      };
+    } catch (err) {
+      return {
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message,
+      };
+    }
+  }
+
+  validateAcessTokenData(email, password) {
+    if (!email || !password) {
+      throw new UserException(
+        httpStatus.UNAUTHORIZED,
+        "Email and password must be informed."
+      );
+    }
+  }
+
+  async validatePassword(password, hashPassword) {
+    if (!(await bcrypt.compare(password, hashPassword))) {
+      throw new UserException(
+        httpStatus.UNAUTHORIZED,
+        "Password doesn't match."
+      );
+    }
   }
 }
 
