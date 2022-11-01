28aca80b94ec8b6ab6d18e70e7a8a548117c5d34
Rebeca Veras <rebecaverasa@gmail.com>
2022-11-01 14:58:57 -0300

    Setting the connection with MongoDB

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,18 +1,21 @@
 import express from "express";
 
+import { connect } from "./src/config/db/mongoDbConfig.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-app.get('/api/status', (req, res) => {
-    return res.status(200).json({
-        service: "Sales-API",
-        status: "up",
-        httpStatus:200,
-    });
+connect();
+
+app.get("/api/status", (req, res) => {
+  return res.status(200).json({
+    service: "Sales-API",
+    status: "up",
+    httpStatus: 200,
+  });
 });
 
 app.listen(PORT, () => {
-    console.info(`Server started successfully at port ${PORT}`);
+  console.info(`Server started successfully at port ${PORT}`);
 });
-
---
+++ sales-api/src/config/db/mongoDbConfig.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+import { MONGO_DB_URL } from "../constants/secrets.js";
+
+export function connect() {
+  mongoose.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+  });
+  mongoose.connection.on("connected", function () {
+    console.info("The application has connected with MongoDB successfully!");
+  });
+  mongoose.connection.on("error", function () {
+    console.error("The application didnt connected with MongoDB.");
+  });
+}
