4c3dc0ed8ea426dcebe27c7ec8482cf7dee144c4
Rebeca Veras <rebecaverasa@gmail.com>
2022-11-10 10:17:17 -0300

    Initial data created

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,17 +1,15 @@
 import express from "express";
 
 import { connect } from "./src/config/db/mongoDbConfig.js";
-import Order from "./src/modules/sales/model/Order.js";
-
+import { createInitialData } from "./src/config/db/initialData.js";
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
 connect();
+createInitialData();
 
 app.get("/api/status", async (req, res) => {
-  let teste = await Order.find();
-  console.log(teste);
   return res.status(200).json({
     service: "Sales-API",
     status: "up",
---
+++ sales-api/src/config/db/initialData.js
@@ -0,0 +1,51 @@
+import Order from "../../modules/sales/model/Order.js";
+
+export async function createInitialData() {
+  await Order.collection.drop();
+  await Order.create({
+    products: [
+      {
+        productId: 1001,
+        quantity: 2,
+      },
+      {
+        productId: 1002,
+        quantity: 1,
+      },
+      {
+        productId: 1003,
+        quantity: 1,
+      },
+    ],
+    user: {
+      id: "duhqed7qdh7d7q143",
+      name: "User Test",
+      email: "usertest@gmail.com",
+    },
+    status: "APPROVED",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  await Order.create({
+    products: [
+      {
+        productId: 1001,
+        quantity: 4,
+      },
+      {
+        productId: 1003,
+        quantity: 2,
+      },
+    ],
+    user: {
+      id: "2389edjui2389d",
+      name: "User Test 2",
+      email: "usertest2@gmail.com",
+    },
+    status: "REJECTED",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  let initialData = await Order.find();
+  console.info(`Initial data was created: ${JSON.stringify(initialData)}`);
+}
