c7e944ff1db2b5f11722f49e0f4449d93012cde2
Rebeca <rebecaverasa@gmail.com>
2023-03-02 15:29:40 -0300

    db_user changed to admin

--- auth-api/scr/config/constants/secrets.js
+++ auth-api/scr/config/constants/secrets.js
@@ -6,6 +6,6 @@ export const API_SECRET = env.API_SECRET
 
 export const DB_HOST = env.DB_HOST ? env.DB_HOST : "localhost";
 export const DB_NAME = env.DB_NAME ? env.DB_NAME : "auth-db";
-export const DB_USER = env.DB_USER ? env.DB_USER : "postgres";
+export const DB_USER = env.DB_USER ? env.DB_USER : "admin";
 export const DB_PASSWORD = env.DB_PASSWORD ? env.DB_PASSWORD : "123456";
 export const DB_PORT = env.DB_PORT ? env.DB_PORT : "5432";
